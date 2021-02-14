d2d2169f4ef4f80dfbd227dbc58e860dc08b6ddd
Bilge Kağan Özkurt <bilgekaganozkurt@gmail.com>
2021-02-14 21:07:45 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal ıCardal)
+        {
+            _carDal= ıCardal;
+        }
+
+
+        public List<Car> GetAll()
+        {
+            return  _carDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Id : " + car.Id);
+                Console.WriteLine("BrandId : " +car.BrandId);
+                Console.WriteLine("ColorId : " +car.ColorId);
+                Console.WriteLine("ModelYear : " + car.ModelYear);
+                Console.WriteLine("DailyPrice : " + car.DailyPrice);
+                Console.WriteLine("Features : " +car.Description);
+                Console.WriteLine("--------------");
+                //Kendim fazlalık olarak eklediğim bölümler var.
+            }
+        }
+    }
+}
